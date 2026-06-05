a12f80181a20d1e696024994691fff9f8bf2e91f
LUKEWOT <wya24xpu@uea.ac.uk>
2026-06-05 10:37:39 +0100

    "functionality added for foodbank login"

---
+++ app/src/main/java/com/example/cropconnect/activities/foodbank/FOOD_Dashboard.java
@@ -0,0 +1,14 @@
+package com.example.cropconnect.activities.foodbank;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.cropconnect.R;
+
+public class FOOD_Dashboard extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
+
