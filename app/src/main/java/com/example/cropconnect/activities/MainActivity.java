fa9553724ec9edceb0185259bc05e0e31f0bd5a8
Oliver Ward <wardo6187@gmail.com>
2026-06-05 19:31:33 +0100

    Homescreen translation functionality implemented

--- app/src/main/java/com/example/cropconnect/activities/MainActivity.java
+++ app/src/main/java/com/example/cropconnect/activities/MainActivity.java
@@ -15,6 +15,9 @@ import com.google.android.material.button.MaterialButton;
 import android.widget.ImageButton;
 import android.widget.PopupMenu;
 
+import android.content.res.Configuration;
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -32,8 +35,24 @@ public class MainActivity extends AppCompatActivity {
 
         btnLanguage.setOnClickListener(v -> {
             PopupMenu popupMenu = new PopupMenu(this, btnLanguage);
-            popupMenu.getMenu().add("English");
-            popupMenu.getMenu().add("Urdu");
+
+            popupMenu.getMenu().add(0, 1, 0, getString(R.string.english));
+            popupMenu.getMenu().add(0, 2, 1, getString(R.string.urdu));
+
+            popupMenu.setOnMenuItemClickListener(item -> {
+                if (item.getItemId() == 1) {
+                    setLocale("en");
+                    return true;
+                }
+
+                if (item.getItemId() == 2) {
+                    setLocale("ur");
+                    return true;
+                }
+
+                return false;
+            });
+
             popupMenu.show();
         });
 
@@ -51,5 +70,19 @@ public class MainActivity extends AppCompatActivity {
         btnGuest.setOnClickListener(v -> {
             startActivity(new Intent(this, Maps.class));
         });
+    }
+    private void setLocale(String languageCode) {
+        Locale locale = new Locale(languageCode);
+        Locale.setDefault(locale);
+
+        Configuration configuration = getResources().getConfiguration();
+        configuration.setLocale(locale);
+
+        getResources().updateConfiguration(
+                configuration,
+                getResources().getDisplayMetrics()
+        );
+
+        recreate();
     }
 }
